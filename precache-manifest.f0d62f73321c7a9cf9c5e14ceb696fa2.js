62cb274a7b73fb43ff8f6ad6ed99affa190c0c29
Piotr Kuniniec <p.kunio@icloud.com>
2020-03-09 13:57:56 +1100

    Updates

---
+++ precache-manifest.f0d62f73321c7a9cf9c5e14ceb696fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d886be806ec9e7fdd893f7c5c66484",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a94f5c23f3b9886fff19",
+    "url": "/monsters-rolodex/static/css/main.cac85de3.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94f5c23f3b9886fff19",
+    "url": "/monsters-rolodex/static/js/main.5c1efec7.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
